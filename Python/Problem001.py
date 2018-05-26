81d2aa86749fa95c10b59063dd0e85e545231ce0
ch <compuprac@gmail.com>
2018-05-26 00:48:42 -0500

    Add method is_multiple()

--- Python/Problem001.py
+++ Python/Problem001.py
@@ -3,3 +3,7 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
+
+#deterime if the number is a multiple of multiple.
+def is_multiple(number, multiple):
+	return number % multiple == 0
