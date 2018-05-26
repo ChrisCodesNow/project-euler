abea71dc3a811748e7bd8ff932bd38fdaf52dec4
ch <compuprac@gmail.com>
2018-05-26 01:07:14 -0500

    Add find sum routine.

--- Python/Problem001.py
+++ Python/Problem001.py
@@ -4,6 +4,17 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
-#deterime if the number is a multiple of multiple.
+# Deterime if the number is a multiple of multiple.
 def is_multiple(number, multiple):
 	return number % multiple == 0
+
+
+
+# Find sum
+limit = 1000
+sum = 0
+for i in range(1, limit):
+	if is_multiple(i, 3) or is_multiple(i, 5):
+		sum += i
+
+print(sum)
