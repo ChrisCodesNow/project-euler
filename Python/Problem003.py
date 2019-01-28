699b179b5af02d9502edb5428f231f66573e0d17
ch <compuprac@gmail.com>
2019-01-27 22:32:33 -0600

    Problem 003 complete

--- Python/Problem003.py
+++ Python/Problem003.py
@@ -3,4 +3,27 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 '''
+from math import sqrt, floor
 
+def is_factor(number, factor):
+	return number % factor == 0
+
+# Recursively get max prime factor of the number
+def max_prime_factor(number):
+
+	factors = set()	# store all factors
+
+	# GET ALL FACTORS FROM 1 TO SQRT(NUMBER)
+	max_possible_factor = floor(sqrt(number))
+	for i in range(1, max_possible_factor + 1):
+		if(is_factor(number, i)):
+			factor1 = i
+			factor2 = number / i
+			
+	print(sorted(factors))
+
+	# GET RID OF EVEN NON-PRIME FACTORS
+
+
+# MAIN
+print(max_prime_factor(100))		
